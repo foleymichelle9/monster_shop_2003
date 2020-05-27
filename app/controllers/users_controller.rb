cbd159d930926fb55d86da6538d317c34bf0c6a9
Mariana Cid <cidmariana5@gmail.com>
2020-05-27 14:37:05 -0600

    controller model created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,36 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    session[:user_id] = @user.id
-    flash[:success] = "Welcome #{@user.name}! You are now registered and logged in!"
-    redirect_to "/profile"
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome #{@user.name}! You are now registered and logged in!"
+      redirect_to "/profile"
+    elsif email_exists?
+      flash[:error] = "Email has already been taken"
+      render :new
+    else
+      flash[:notice] = "#{missing_params(user_params).join(", ")} can't be blank, please try again."
+      render :new
+    end
   end
 
   private
 
   def user_params
     params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
+  end
+
+  def email_exists?
+    User.where(email: params[:email]).exists?
+  end
+
+  def missing_params(user_params)
+    missing_params = []
+    user_params.each do |key, value|
+      if value == ""
+        missing_params << "#{key}"
+      end
+    end
+    missing_params
   end
 end 
