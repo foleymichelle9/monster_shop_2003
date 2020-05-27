76302bc64d0690d70736efced095c6184a535a6e
Mariana Cid <cidmariana5@gmail.com>
2020-05-27 15:47:19 -0600

    refactor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200527043603) do
+ActiveRecord::Schema.define(version: 20200527212017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,10 +74,10 @@ ActiveRecord::Schema.define(version: 20200527043603) do
     t.string "state"
     t.string "zip"
     t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
   end
 
   add_foreign_key "item_orders", "items"
