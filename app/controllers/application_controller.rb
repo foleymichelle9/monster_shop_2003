ba1a994aed36f8fbebd848463cdd76dadd430367
Ross Perry <perryr16@gmail.com>
2020-05-27 16:18:13 -0600

    merge conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,10 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
-<<<<<<< HEAD
-end
-=======
+
   def current_user
     user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
->>>>>>> 6bc6cbe2022002ed5ade01e0e23f9ae76ac98691
+
