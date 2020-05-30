832d02636c3fa1b51dc358b4bc40b59f43b7d136
Ross Perry <perryr16@gmail.com>
2020-05-30 14:24:25 -0600

    spelling

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe User, type: :model do
     it {should validate_uniqueness_of :email}
     it {should validate_presence_of :password}
   end
-  describe "relationshios" do
+  describe "relationships" do
     it {should have_many :orders}
   end
   
