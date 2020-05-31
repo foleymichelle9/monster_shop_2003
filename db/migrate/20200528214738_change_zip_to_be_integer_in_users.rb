939ed7cbc75f894fe4c558a76890ad31730f53de
michelle <foleymichelle9@gmail.com>
2020-05-30 18:34:21 -0600

    Add admin dashboard controller

--- db/migrate/20200528214738_change_zip_to_be_integer_in_users.rb
+++ db/migrate/20200528214738_change_zip_to_be_integer_in_users.rb
@@ -1,9 +1,5 @@
 class ChangeZipToBeIntegerInUsers < ActiveRecord::Migration[5.1]
   def change
-<<<<<<< HEAD
-    change_column :users, :zip, 'integer USING CAST (zip AS integer)'
-=======
     change_column :users, :zip, 'integer USING CAST(zip AS integer)'
->>>>>>> master
   end
 end
