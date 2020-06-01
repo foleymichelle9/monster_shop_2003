e87df359d983282c115d9265cd7ffc38a5ccf6e6
Mariana Cid <cidmariana5@gmail.com>
2020-06-01 15:59:17 -0600

    refactor for creation spec

--- spec/features/orders/creation_spec.rb
+++ spec/features/orders/creation_spec.rb
@@ -56,7 +56,9 @@ RSpec.describe("Order Creation") do
 
       new_order = Order.last
 
-      expect(current_path).to eq("/orders/#{new_order.id}")
+      expect(current_path).to eq("/profile/orders")
+      click_link("#{Order.last.id}")
+      expect(current_path).to eq("/profile/orders/#{Order.last.id}") 
 
       within '.shipping-address' do
         expect(page).to have_content(name)
