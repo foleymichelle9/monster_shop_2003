039eade57222d45cbc4ac1c15bd96d27461ed043
Derek Borski <derek.borski@gmail.com>
2020-06-01 12:41:12 -0600

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Review.destroy_all
 Merchant.destroy_all
 Order.destroy_all
 ItemOrder.destroy_all
@@ -23,17 +24,32 @@ helmet = bike_shop.items.create(name: "Helmet", description: "Safety first", pri
 bicycle = bike_shop.items.create(name: "Bicycle", description: "Mountain bike", price: 800, image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/canyon-strive-29-tested-1577145399.jpg?crop=0.889xw:1.00xh;0.0705xw,0&resize=640:*", inventory: 3)
 tire_iron = bike_shop.items.create(name: "Tire Iron", description: "Does the job", price: 10, image: "https://www.somafab.com/wp-content/uploads/2011/06/tirelever_core_full_lever4c1.jpg", inventory: 0, active?: false)
 
+#bike_shop item reviews
+review1 = tire.reviews.create(title: "Great tire", content: "Will buy this tire again", rating: 8)
+review2 = helmet.reviews.create(title: "Cool helmet", content: "Very safe", rating: 7)
+
 #dog_shop items
 pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
 dog_food = dog_shop.items.create(name: "Dog Food", description: "Super tasty", price: 30, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgRJErkeYhfadwU8k2Jux0Hhp7KsvijgO61Ia2T__NWpPl3cLZ&usqp=CAU", inventory: 15)
 dog_pram = dog_shop.items.create(name: "Dog pram", description: "Carry your dog in style", price: 200, image: "https://assets.petco.com/petco/image/upload/f_auto,q_auto,t_ProductDetail-large/2327664-center-2", inventory: 0, active?: false)
 
+#dog_shop item reviews
+review3 = pull_toy.reviews.create(title: "Crappy pull toy", content: "Dog hates this", rating: 2)
+review4 = dog_food.reviews.create(title: "Mediocore dog food", content: "Dog doesn't love it or hate it", rating: 5)
+review5 = dog_food.reviews.create(title: "Good, not great dog food", content: "Acceptable for my dog", rating: 7)
+
 #computer_shop items
 laptop = computer_shop.items.create(name: "Laptop", description: "Fastest computer around!", price: 1000, image: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6356/6356671_sd.jpg", inventory: 20)
 keyboard = computer_shop.items.create(name: "Keyboard", description: "Bluetooth keyboard", price: 50, image: "https://images-na.ssl-images-amazon.com/images/I/81PLqxtrJ3L._AC_SL1500_.jpg", inventory: 15)
 mouse = computer_shop.items.create(name: "Mouse", description: "Bluetooth mouse", price: 40, image: "https://www.staples-3p.com/s7/is/image/Staples/s1020457_sc7?wid=512&hei=512", inventory: 13)
 carrying_case = computer_shop.items.create(name: "Laptop carrying case", description: "Keep your laptop safe", price: 75, image: "https://static.bhphoto.com/images/images2500x2500/1422621364_1116114.jpg", inventory: 13, active?: false)
+
+#computer_shop reviews
+review6 = laptop.reviews.create(title: "Great laptop", content: "Love it", rating: 9)
+review7 = laptop.reviews.create(title: "Decent laptop", content: "Love it", rating: 7)
+review8 = keyboard.reviews.create(title: "Decent keyboard", content: "Does the job", rating: 7)
+review9 = mouse.reviews.create(title: "Decent mouse", content: "Does the job", rating: 6)
 
 #Users
 regular1 = User.create!(name: "User Name1", address: "user address1", city: "user city", state: "state", zip: "user zip", email: "user1", password: "user1", role: 0)
