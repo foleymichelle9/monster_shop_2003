2b5940a7445d824f3db6f747a3fc6adadf6d34e9
Derek Borski <derek.borski@gmail.com>
2020-06-01 21:00:30 -0600

    Add pending_merchant_orders model method and spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -34,7 +34,37 @@ describe Order, type: :model do
     it '#total items' do
       expect(@order_1.total_items).to eq(5)
     end
- 
-    
+  end
+  describe 'class methods' do
+    it 'pending_merchant_orders' do
+
+      @merchant1 = create(:merchant)
+      @merchant2 = create(:merchant)
+      @merchant3 = create(:merchant)
+      
+      @item1 = create(:item, merchant_id: @merchant1.id)
+      @item2 = create(:item, merchant_id: @merchant1.id)
+      @item3 = create(:item, merchant_id: @merchant1.id)
+      @item4 = create(:item, merchant_id: @merchant1.id)
+      @item5 = create(:item, merchant_id: @merchant2.id)
+      @item6 = create(:item, merchant_id: @merchant1.id)
+      @item7 = create(:item, merchant_id: @merchant3.id)
+
+      @order1 = create(:order, status: "pending")
+      @order2 = create(:order, status: "packaged")
+      @order3 = create(:order, status: "pending")
+      @order4 = create(:order, status: "pending")
+      @order5 = create(:order, status: "pending")
+
+      ItemOrder.create!(order: @order1, item: @item1, price: @item1.price, quantity: 1, status: "unfulfilled")
+      ItemOrder.create!(order: @order1, item: @item2, price: @item1.price, quantity: 1, status: "unfulfilled")
+      ItemOrder.create!(order: @order2, item: @item3, price: @item3.price, quantity: 1, status: "fulfilled")
+      ItemOrder.create!(order: @order3, item: @item4, price: @item4.price, quantity: 1, status: "unfulfilled")
+      ItemOrder.create!(order: @order3, item: @item1, price: @item1.price, quantity: 1, status: "unfulfilled")
+      ItemOrder.create!(order: @order4, item: @item5, price: @item5.price, quantity: 1, status: "unfulfilled")
+      ItemOrder.create!(order: @order5, item: @item7, price: @item7.price, quantity: 1, status: "unfulfilled")
+
+      expect(Order.pending_merchant_orders(@merchant1.id)).to eq([@order1, @order3])
+    end
   end
 end
