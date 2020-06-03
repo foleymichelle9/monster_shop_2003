35590fe5247b0d8996cafeb8e7444b1ad637802a
Mariana Cid <cidmariana5@gmail.com>
2020-06-03 14:35:20 -0600

    admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,4 +3,10 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.all
   end 
+
+  def show
+    @user = User.find(params[:id])
+    @orders = Order.all
+  end
+  
 end
